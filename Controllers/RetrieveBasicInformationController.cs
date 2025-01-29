483eb15634e6991ca2873541f6a998831f7de64d
takinnuoye <takinnuoye@AHSB-IT-LT84>
2025-01-29 16:23:54 +0100

    Add project files.

---
+++ Controllers/RetrieveBasicInformationController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG_STAGE_ZERO_TASK.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RetrieveBasicInformationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var response = new
+            {
+                email = "akinnuoyet@gmail.com",
+                current_datetime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                github_url = "https://github.com/Takinnuoye5/hng-stage-zer0-task"
+            };
+
+            return Ok(response);
+        }
+    }
+    
+}
